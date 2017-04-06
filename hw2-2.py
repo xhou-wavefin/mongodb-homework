7612a116b1f6d12508c189beaf0f8ebf0b5033ac
Xinyu Hou <xinyu@boltonsoftware.com>
2017-04-06 21:11:55 +0100

    Remove homework min score for each student

--- hw2-2.py
+++ hw2-2.py
@@ -1,5 +1,5 @@
 import pymongo
-
+import sys
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -8,4 +8,37 @@ db = client['students']
 
 grades = db['grades']
 
-print grades.find_one()
+cur =  grades.find({'type' : 'homework'})
+cur.sort([('student_id',pymongo.ASCENDING), ('score', pymongo.ASCENDING)])
+
+# remove the lowest homework score
+preStudentId = 0
+minScore = 1000
+minScoreId = ''
+deleteId = []
+
+for entry in cur:
+
+	if entry['student_id'] != preStudentId:
+		# we are in a new student
+		# process the previous student
+		deleteId.append(minScoreId)
+
+		preStudentId = entry['student_id'] 
+		minScore = entry['score']
+		minScoreId = entry['_id']
+	else:
+		if entry['score'] < minScore:
+			minScore = entry['score']
+			minScoreId = entry['_id']
+
+deleteId.append(minScoreId)
+for id in deleteId:
+	grades.delete_one({'_id' : id})
+
+sanity = 0
+for entry in cur:
+	print entry
+	sanity += 1
+	if sanity > 4:
+		break
